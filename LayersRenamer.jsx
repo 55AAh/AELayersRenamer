d5d2f6fae138ee73a34f509fc42f5912144317c4
Kostia Kulik <kostya.kulik@gmail.com>
2024-04-05 15:04:03 +0300

    Implement file storage

--- LayersRenamer.jsx
+++ LayersRenamer.jsx
@@ -2,6 +2,14 @@ var scriptName = "LayersRenamer";
 var scriptVersion = "0.1";
 
 //
+// ---------- MINIFIED json2.js ----------
+//
+
+// prettier-ignore
+// cSpell: disable-next-line
+"object"!=typeof JSON&&(JSON={}),function(){"use strict";var gap,indent,meta,rep,rx_one=/^[\],:{}\s]*$/,rx_two=/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,rx_three=/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,rx_four=/(?:^|:|,)(?:\s*\[)+/g,rx_escapable=/[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,rx_dangerous=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;function f(t){return t<10?"0"+t:t}function this_value(){return this.valueOf()}function quote(t){return rx_escapable.lastIndex=0,rx_escapable.test(t)?'"'+t.replace(rx_escapable,function(t){var e=meta[t];return"string"==typeof e?e:"\\u"+("0000"+t.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+t+'"'}function str(t,e){var n,o,f,u,r,$=gap,i=e[t];switch(i&&"object"==typeof i&&"function"==typeof i.toJSON&&(i=i.toJSON(t)),"function"==typeof rep&&(i=rep.call(e,t,i)),typeof i){case"string":return quote(i);case"number":return isFinite(i)?String(i):"null";case"boolean":case"null":return String(i);case"object":if(!i)return"null";if(gap+=indent,r=[],"[object Array]"===Object.prototype.toString.apply(i)){for(n=0,u=i.length;n<u;n+=1)r[n]=str(n,i)||"null";return f=0===r.length?"[]":gap?"[\n"+gap+r.join(",\n"+gap)+"\n"+$+"]":"["+r.join(",")+"]",gap=$,f}if(rep&&"object"==typeof rep)for(n=0,u=rep.length;n<u;n+=1)"string"==typeof rep[n]&&(f=str(o=rep[n],i))&&r.push(quote(o)+(gap?": ":":")+f);else for(o in i)Object.prototype.hasOwnProperty.call(i,o)&&(f=str(o,i))&&r.push(quote(o)+(gap?": ":":")+f);return f=0===r.length?"{}":gap?"{\n"+gap+r.join(",\n"+gap)+"\n"+$+"}":"{"+r.join(",")+"}",gap=$,f}}"function"!=typeof Date.prototype.toJSON&&(Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+f(this.getUTCMonth()+1)+"-"+f(this.getUTCDate())+"T"+f(this.getUTCHours())+":"+f(this.getUTCMinutes())+":"+f(this.getUTCSeconds())+"Z":null},Boolean.prototype.toJSON=this_value,Number.prototype.toJSON=this_value,String.prototype.toJSON=this_value),"function"!=typeof JSON.stringify&&(meta={"\b":"\\b","	":"\\t","\n":"\\n","\f":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"},JSON.stringify=function(t,e,n){var o;if(gap="",indent="","number"==typeof n)for(o=0;o<n;o+=1)indent+=" ";else"string"==typeof n&&(indent=n);if(rep=e,e&&"function"!=typeof e&&("object"!=typeof e||"number"!=typeof e.length))throw Error("JSON.stringify");return str("",{"":t})}),"function"!=typeof JSON.parse&&(JSON.parse=function(text,reviver){var j;function walk(t,e){var n,o,f=t[e];if(f&&"object"==typeof f)for(n in f)Object.prototype.hasOwnProperty.call(f,n)&&(void 0!==(o=walk(f,n))?f[n]=o:delete f[n]);return reviver.call(t,e,f)}if(text=String(text),rx_dangerous.lastIndex=0,rx_dangerous.test(text)&&(text=text.replace(rx_dangerous,function(t){return"\\u"+("0000"+t.charCodeAt(0).toString(16)).slice(-4)})),rx_one.test(text.replace(rx_two,"@").replace(rx_three,"]").replace(rx_four,"")))return j=eval("("+text+")"),"function"==typeof reviver?walk({"":j},""):j;throw SyntaxError("JSON.parse")})}();
+
+//
 // ---------- UI HELPER FUNCTIONS ----------
 //
 
@@ -204,6 +212,190 @@ function applyTemplate(template) {
 // ---------- PLUGIN UI ----------
 //
 
+var UID_templateCount = 0;
+// Because templates are stored in a dictionary, we need a convenient way to assign a runtime-unique id to each Template
+// This uses a static counter variable, and stores id Template object itself as attribute
+// This way, if we need to delete template object from a dictionary, we easily get its key
+function UID_get(template) {
+    if (template.UID === undefined) {
+        UID_templateCount += 1;
+        template.UID = UID_templateCount;
+    }
+
+    return template.UID;
+}
+
+// Deep copy function
+// This is needed to prevent modification of the original template
+function copyToTemplate(source, target) {
+    target.name = source.name;
+    target.color = source.color;
+}
+
+// This function ensures that file is selected, then opens it with the required R/W mode
+function openFile(save) {
+    const sectionTag = scriptName;
+    const keyTag = "dataFilePath";
+    function getPathFromPrefs() {
+        if (app.settings.haveSetting(sectionTag, keyTag)) {
+            // Load path from saved Preferences
+            var dataFilePath = app.settings.getSetting(sectionTag, keyTag);
+
+            if (!dataFilePath) {
+                return undefined;
+            }
+
+            // Create File object
+            var file = File(dataFilePath);
+
+            return file;
+        } else {
+            return undefined;
+        }
+    }
+
+    function savePathToPrefs(file) {
+        app.settings.saveSetting(sectionTag, keyTag, file.fsName);
+    }
+
+    function saveUndefToPrefs(file) {
+        app.settings.saveSetting(sectionTag, keyTag, "");
+    }
+
+    function getPathFromUser() {
+        if (save) {
+            var file = File.saveDialog(
+                "Select file to save templates to",
+                "JSON:*.json,All files:*.*"
+            );
+        } else {
+            var file = File.openDialog(
+                "Select file to load templates from",
+                "JSON:*.json,All files:*.*"
+            );
+        }
+
+        if (!file) {
+            return undefined;
+        }
+
+        // Correct file extension if needed
+        if (!/\.(json)$/.test(file.name)) {
+            file.rename(file.name + ".json");
+        }
+
+        return file;
+    }
+
+    function tryToOpen(file) {
+        mode = save ? "w" : "r";
+        if (file.open(mode)) {
+            return file;
+        } else {
+            alert(
+                "Unable to open " + scriptName + " file '" + file.fsName + "'!"
+            );
+            return undefined;
+        }
+    }
+
+    // If there is some path in Prefs
+    var file = getPathFromPrefs();
+    if (file) {
+        if (tryToOpen(file)) {
+            return file;
+        }
+    }
+
+    // Prompt user to specify path
+    file = getPathFromUser();
+    if (file && tryToOpen(file)) {
+        savePathToPrefs(file);
+        return file;
+    }
+
+    saveUndefToPrefs();
+
+    return undefined;
+}
+
+function loadFromFile() {
+    // Obtaining file object
+    var file = openFile(false);
+    if (!file) {
+        alert("Templates were not loaded from a file!");
+        return {};
+    }
+
+    // Reading (file is already open)
+    dataStr = file.read();
+    file.close();
+
+    // Deserializing
+    try {
+        data = JSON.parse(dataStr);
+
+        if (data.version != scriptVersion) {
+            alert(
+                "Version mismatch:\n" +
+                    "\nscript version is " +
+                    scriptVersion +
+                    "\nversion in file is " +
+                    data.version +
+                    "\n\nTrying to load anyway..."
+            );
+        }
+
+        newTemplates = {};
+        loadedTemplates = data.templates;
+        for (var i in loadedTemplates) {
+            loadedTemplate = loadedTemplates[i];
+
+            // Copying template to assign new UID
+            newTemplate = {};
+            copyToTemplate(loadedTemplate, newTemplate);
+
+            // Adding new template to the collection
+            newTemplate_UID = UID_get(newTemplate);
+            newTemplates[newTemplate_UID] = newTemplate;
+        }
+
+        return newTemplates;
+    } catch (e) {
+        alert(
+            "Error parsing " +
+                scriptName +
+                " file '" +
+                file.fsName +
+                "':\n" +
+                e.toString()
+        );
+    }
+
+    return {};
+}
+
+function saveToFile(templates) {
+    // Obtaining file object
+    var file = openFile(true);
+    if (!file) {
+        alert("Templates were not saved to a file!");
+        // Return the same templates
+        return templates;
+    }
+
+    // Serializing
+    data = {
+        version: scriptVersion,
+        templates: templates,
+    };
+    dataStr = JSON.stringify(data, undefined, 4);
+
+    // Writing (file is already open)
+    file.write(dataStr);
+    file.close();
+}
+
 // This shows a dialog that allows user to
 //
 // 1) isNew = true:     Create a new template
@@ -233,10 +425,6 @@ function buildUI_editTemplateDlg_show(
         title = "Edit template '" + template.name + "'";
     }
 
-    function copyToTemplate(source, target) {
-        target.name = source.name;
-        target.color = source.color;
-    }
     tempoTemplate = {};
     copyToTemplate(template, tempoTemplate);
 
@@ -363,19 +551,6 @@ function buildUI_editTemplateDlg_show(
     dlg.show();
 }
 
-var UID_templateCount = 0;
-// Because templates are stored in a dictionary, we need a convenient way to assign a runtime-unique id to each Template
-// This uses a static counter variable, and stores id Template object itself as attribute
-// This way, if we need to delete template object from a dictionary, we easily get its key
-function UID_get(template) {
-    if (template.UID === undefined) {
-        UID_templateCount += 1;
-        template.UID = UID_templateCount;
-    }
-
-    return template.UID;
-}
-
 // This shows a scripts main panel (dockable)
 function buildUI_mainPanel(panel) {
     panel.preferredSize = { width: 205, height: 240 };
@@ -393,13 +568,13 @@ function buildUI_mainPanel(panel) {
     // we delete the panel itself (one child), and then recreate it in this group
     var templateButtonsGroup = panel.add("group", [5, 40, 205, 240]);
 
-    // This is the dictionary that stores template objects
+    // This is the dictionary that stores template objects, we retrieve it from saved copy
     // Template's UID is used as a key to allow easy deleting
     // For details see the UID_get() function's comments
-    var templates = {};
+    var templates = loadFromFile();
 
     // This deletes the existing template buttons, and (re)creates them from the templates{} dict
-    function re_build_buttons() {
+    function recreateButtons() {
         prev_panel = templateButtonsGroup.children[0];
         if (prev_panel !== undefined) {
             templateButtonsGroup.remove(prev_panel); // Delete the existing panel
@@ -453,7 +628,10 @@ function buildUI_mainPanel(panel) {
                             function (_same_template) {
                                 // The original template object will be modified
                                 // Redraw template buttons list
-                                re_build_buttons();
+                                recreateButtons();
+
+                                // Update saved copy
+                                saveToFile(templates);
                             },
                             // onDelete callback
                             function () {
@@ -462,8 +640,12 @@ function buildUI_mainPanel(panel) {
                                     right_clicked_template
                                 );
                                 delete templates[right_clicked_template_UID];
-                                // Redraw template buttons list
-                                re_build_buttons();
+
+                                // Update buttons list
+                                recreateButtons();
+
+                                // Update saved copy
+                                saveToFile(templates);
                             }
                         );
                     }
@@ -488,11 +670,19 @@ function buildUI_mainPanel(panel) {
             newTemplate,
             true, // Template is new
             function () {
+                // Adding new template to the collection
                 templates[UID_get(newTemplate)] = newTemplate;
-                re_build_buttons();
+
+                // Update buttons list
+                recreateButtons();
+
+                // Update saved copy
+                saveToFile(templates);
             }
         );
     };
+
+    recreateButtons();
 }
 
 //
